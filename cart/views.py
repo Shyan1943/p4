0450e8b3c2d72d1d3b79a3ecfe68ad9afb7e7a57
Shyan1943 <shyan_1943@hotmail.com>
2020-08-08 03:55:01 +0000

    Added 'C' route for add a program to shopping cart

--- cart/views.py
+++ cart/views.py
@@ -5,5 +5,16 @@ from programs.models import Program
 
 # Create your views here.
 def add_to_cart(request, program_id):
-    print(program_id)
+    cart = request.session.get("shopping_cart", {})
+    if program_id not in cart:
+        program = get_object_or_404(Program, pk=program_id)
+        cart[program_id] = {
+            "id": program_id,
+            "title": program.title,
+            "fees": 500,
+            'qty': 1
+        }
+    else:
+        cart[program_id]['qty'] += 1
+    request.session["shopping_cart"] = cart
     return HttpResponse("Pro added")
