5b57b7bb6fa01b89bef94700e6f955262da503cf
Shyan1943 <shyan_1943@hotmail.com>
2020-08-12 15:09:25 +0000

    Adding comments msg to cart/views.py

--- cart/views.py
+++ cart/views.py
@@ -1,17 +1,19 @@
-from django.shortcuts import render, HttpResponse, get_object_or_404, redirect, reverse
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from programs.models import Program
-# import json
-# from django.core.serializers.json import DjangoJSONEncoder
 
 
 # Create your views here.
 @ login_required
 def view_cart(request):
+
+    # loading the content of the 'shopping_cart' from the session
     cart = request.session.get("shopping_cart", {})
     total = 0
     for k, v in cart.items():
+        # have to convert back to float because
+        # session can only store strings
         total += float(v["fees"]) * int(v["qty"])
 
     return render(request, "cart/view_cart.template.html", {
@@ -22,9 +24,16 @@ def view_cart(request):
 
 @ login_required
 def add_to_cart(request, program_id):
+
+    # the cart object is a dictionary
     cart = request.session.get("shopping_cart", {})
+
+    # check if the program_id I want to add already exists inside the cart
+    # if the program is not in the shopping cart
     if program_id not in cart:
         program = get_object_or_404(Program, pk=program_id)
+
+        # add the program to cart
         cart[program_id] = {
             "id": program_id,
             "title": program.title,
@@ -36,7 +45,10 @@ def add_to_cart(request, program_id):
         messages.success(
             request, f"Added program '{program.title}' to the shopping cart")
     else:
+        # if the program already exists in the cart
         cart[program_id]["qty"] += 1
+
+    # save the shopping cart back to session
     request.session["shopping_cart"] = cart
     return redirect(reverse(view_cart))
 
@@ -45,7 +57,11 @@ def add_to_cart(request, program_id):
 def remove_from_cart(request, program_id):
     cart = request.session["shopping_cart"]
     if program_id in cart:
+
+        # removew from the cart
         del cart[program_id]
+
+        # save back the shopping cart into the session
         request.session["shipping_cart"] = cart
         messages.success(request, "The program has been removed")
     return redirect(reverse("view_cart"))
@@ -53,10 +69,16 @@ def remove_from_cart(request, program_id):
 
 @ login_required
 def update_cart_quantity(request, program_id):
+
+    # get the shopping cart
     cart = request.session["shopping_cart"]
     if program_id in cart:
+
+        # update the quantity
         cart[program_id]['qty'] = request.POST['qty']
         cart[program_id]['total_fees'] = int(request.POST['qty']) * float(cart[program_id]['fees'])
+
+        # save the cart back into the session
         request.session["shopping_cart"] = cart
         messages.success(request, f"Quantity for {cart[program_id]['title']} has been updated")
         return redirect(reverse("view_cart"))
