0900c5444da67275624cd97c41850fa555467fd7
Shyan1943 <shyan_1943@hotmail.com>
2020-08-12 10:24:17 +0000

    Adding comments to 'home/forms.py'

--- home/forms.py
+++ home/forms.py
@@ -1,8 +1,17 @@
 from django import forms
-from dgs.models import Dg, IMDGCode
+from dgs.models import IMDGCode
 
 
+# SearchForm inherits from `forms.Form`, not `forms.ModelForm`
+# This is because the search form doesn't have any models
+# meaning it does not refer to any tables in our SQL database
 class SearchForm(forms.Form):
+
+    # the requried is `False` so that if the user leaves it blank
+    # it will return all the information
     title = forms.CharField(max_length=100, required=False)
+
+    # IMDGCode.objects.all() is eqv to "SELECT * FROM IMDGCode"
+    # take the results of IMDGCode.objects.all and populate the <select>
     imdg_code = forms.ModelChoiceField(queryset=IMDGCode.objects.all(),
                                        required=False)
