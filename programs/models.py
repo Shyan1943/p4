93a868c67fc9573e2440c3bebc9caf93adfcd8c5
Shyan1943 <shyan_1943@hotmail.com>
2020-08-18 00:30:43 +0000

    Styled details_program page

--- programs/models.py
+++ programs/models.py
@@ -10,7 +10,7 @@ class Program(models.Model):
     objectives = models.TextField(blank=False)
     accreditation_and_examination = models.CharField(blank=False,
                                                      max_length=255)
-    fees = models.DecimalField(max_digits=10, decimal_places=3, blank=False)
+    fees = models.DecimalField(max_digits=10, decimal_places=2, blank=False)
 
     def __str__(self):
         return self.title
