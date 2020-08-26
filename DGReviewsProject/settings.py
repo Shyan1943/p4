fc8e485328cfbe1b74c85de49a0692361b7f073b
Shyan1943 <shyan_1943@hotmail.com>
2020-08-26 06:39:53 +0000

    Change the allowed host to heroku deployed site only

--- DGReviewsProject/settings.py
+++ DGReviewsProject/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = '$a*t@r97k0)u_zx)_n5*z_)&p4k5*=crg(b7-ugy0o83m5ddh-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*", "shyan-tgc-pro4.herokuapp.com"]
-# ALLOWED_HOSTS = ["shyan-tgc-pro4.herokuapp.com"]
+ALLOWED_HOSTS = ["shyan-tgc-pro4.herokuapp.com"]
 
 
 # Application definition
